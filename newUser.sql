10c73146ef0315203d9883fe98eac3d76a80fa65
taynaradb <taynaraax0@hotmail.com>
2021-04-26 19:06:41 -0300

    Editing

--- newUser.sql
+++ newUser.sql
@@ -4,4 +4,8 @@ USE newUser;
 
 CREATE TABLE tb_user (nome varchar(20), senha varchar(8), checks varchar(10), admins(3));
 
-INSERT INTO tb_user VALUES('Taynara', '1234', 'female', 'sim');
+INSERT INTO tb_user VALUES('taynara', '1234', 'female', 'Sim');
+
+INSERT INTO tb_user VALUES('vitor', '7894', 'male', 'Não');
+
+
